3100da9118256ccd35b624f7556ba557458e16d3
Sushil Namdeo Raut <sraut@sdsu.edu>
2018-12-04 13:47:38 -0800

    Added IFFE in ES6

--- scripts/ImmediatelyInvokedFunctionExpression.js
+++ scripts/ImmediatelyInvokedFunctionExpression.js
@@ -49,3 +49,19 @@ We dont need to call below function explicitly. It gets invoked automatically. H
     }
   }
 )();
+
+
+//IIFE in ES6
+/*
+1) Use let and const instead of variable
+2) Include all the code in {} instead of ()(); in as in ES5
+3) Since let and const are block scoped, they would not be accessible out side of {}, hence data privacy is achieved
+*/
+
+{
+  let a=5;
+  const b=6;
+}
+
+//The following line would give an error
+console.log(a+b);
